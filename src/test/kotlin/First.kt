00972605c6cd1f3992249cbc8590fc8168c0b116
Will Pearson <will.s.pearson+qagoose@gmail.com>
2023-01-11 06:21:54 +1400

    failing test

--- src/test/kotlin/First.kt
+++ src/test/kotlin/First.kt
@@ -1,8 +1,14 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 
 class First {
     @Test
     fun firstTest() {
         println("Hello")
+    }
+
+    @Test
+    fun secondTest() {
+        fail("This test fails");
     }
 }
